f45d856fbe4c4b40f4f6cac0dd84024468dac2d4
Dkhoatp <duckhoatrpt@gmail.com>
2023-04-09 19:00:47 +0700

    complete?

--- mayaIE.py
+++ mayaIE.py
@@ -16,6 +16,8 @@ def createMainMenu():
     cmds.menuItem(l=u"Export", c=exportFunc)
     cmds.menuItem(d=True)
     cmds.menuItem(l=u"Update", c=updateFunc)
+    cmds.menuItem(d=True)
+    cmds.menuItem(l=u"delete History", c=delHistory)
 
 def importFunc(self):
     totalPathName = path + "/" + "TempObject.obj"
@@ -28,17 +30,18 @@ def exportFunc(self):
 
 def updateFunc(self):
     totalPathName = path + "/" + "TempObject.obj"
-    pylysur = "polySurface1"
+    OriginObject = cmds.ls(selection=True)[0]
     selected = cmds.ls(sl=True, long=True) or []
     for eachSel in selected:
         changeObj = eachSel
-    cmds.delete(changeObj, constructionHistory=True)
     cmds.file(totalPathName, type="OBJ", i=True, rdn=True, mbl=True, mnc=True, rpr=":", op="mo=0", ifr=True, itr="combine")
-    cmds.select(pylysur, r=True)
+    cmds.select(OriginObject, r=True)
     cmds.select(changeObj, tgl=True)
-    cmds.blendShape(pylysur, changeObj)
+    cmds.blendShape(OriginObject, changeObj)
     # cmds.blendShape("blendShape1", edit =True, en=1.0)
     cmds.blendShape("blendShape1", edit=True, w=(0, 1.0))
-    cmds.delete(pylysur)
-
+    cmds.delete(changeObj)
+def delHistory(self):
+    os.remove("C:/tempObject/TempObject.obj")
+    os.remove("C:/tempObject/TempObject.mtl")
 createMainMenu()
--- mayaPluginType.py
+++ mayaPluginType.py
@@ -25,31 +25,35 @@ def createMainMenu():
     cmds.menuItem(l=u"Export", c=exportFunc)
     cmds.menuItem(d=True)
     cmds.menuItem(l=u"Update", c=updateFunc)
+    cmds.menuItem(d=True)
+    cmds.menuItem(l=u"delete History", c=delHistory)
 
 def importFunc(self):
     totalPathName = path + "/" + "TempObject.obj"
-    temp = cmds.file(totalPathName, type="OBJ", i=True, rdn=True, mbl=True, mnc=True, rpr=":", op="mo=0",
-                        ifr=True, itr="combine")
+    temp = cmds.file(totalPathName, type="OBJ", i=True, rdn=True, mbl=True, mnc=True, rpr=":", op="mo=0", ifr=True, itr="combine")
 
 def exportFunc(self):
     totalPathName = path + "/" + "TempObject"
-    cmds.file(totalPathName, f=True, op="groups=1;ptgroups=1;materials=1;smoothing=1;normals=1",
-                typ="OBJexport", pr=True, es=True)
+    cmds.file(totalPathName, f=True, op="groups=1;ptgroups=1;materials=1;smoothing=1;normals=1", typ="OBJexport", pr=True, es=True)
+
 
 def updateFunc(self):
     totalPathName = path + "/" + "TempObject.obj"
-    pylysur = "polySurface1"
+    OriginObject = cmds.ls(selection=True)[0]
     selected = cmds.ls(sl=True, long=True) or []
     for eachSel in selected:
         changeObj = eachSel
-    cmds.delete(changeObj, constructionHistory=True)
     cmds.file(totalPathName, type="OBJ", i=True, rdn=True, mbl=True, mnc=True, rpr=":", op="mo=0", ifr=True, itr="combine")
-    cmds.select(pylysur, r=True)
+    cmds.select(OriginObject, r=True)
     cmds.select(changeObj, tgl=True)
-    cmds.blendShape(pylysur, changeObj)
+    cmds.blendShape(OriginObject, changeObj)
     # cmds.blendShape("blendShape1", edit =True, en=1.0)
     cmds.blendShape("blendShape1", edit=True, w=(0, 1.0))
-    cmds.delete(pylysur)
+    cmds.delete(changeObj)
+    
+def delHistory(self):
+    os.remove("C:/tempObject/TempObject.obj")
+    os.remove("C:/tempObject/TempObject.mtl")
 def creator():
     return OpenMayaMPx.asMPxPtr(addIEButton())
 
